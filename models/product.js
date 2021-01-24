f26c7436102c3ef975c070f9b2aed00420670893
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-01-24 12:11:53 +0100

    Installed some dependencies and created data

---
+++ models/product.js
@@ -0,0 +1,10 @@
+class Product {
+    constructor(id, ownerId, title, description, imageUrl, price) {
+        this.id = id
+        this.ownerId = ownerId
+        this.title = title
+        this.description = description
+        this.imageUrl = imageUrl
+        this.price = price
+    }
+}
