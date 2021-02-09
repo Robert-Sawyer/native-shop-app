01e76211e2352c66589aac91254d5f84a7f42731
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-02-09 18:17:26 +0100

    Added orderReducer to App.js and installed dep.

--- App.js
+++ App.js
@@ -6,10 +6,12 @@ import AppLoading from "expo-app-loading";
 import productsReducer from "./store/reducers/products";
 import ShopNavigator from "./navigation/ShopNavigator";
 import cartReducer from "./store/reducers/cart";
+import ordersReducer from "./store/reducers/orders";
 
 const rootReducer = combineReducers({
     products: productsReducer,
     cart: cartReducer,
+    orders: ordersReducer,
 })
 
 const store = createStore(rootReducer)
