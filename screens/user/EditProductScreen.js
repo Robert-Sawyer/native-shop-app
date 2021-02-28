b22ab6996453af8dc445a0d203e16b2039e6fd3e
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-02-28 16:23:49 +0100

    Implemented new functionalities in admin screen

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,20 +1,42 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
 import {ScrollView, View, TextInput, Text, StyleSheet, Platform} from 'react-native'
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 import CustomHeaderButton from "../../components/UI/HeaderButton";
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
+import * as productActions from '../../store/actions/products'
 
 const EditProductScreen = props => {
 
     //pobieram produkty i filtruję je na podstawie parametru
     const prodId = props.navigation.getParam('productId')
-    const editedProduct = useSelector(state => state.products.userProducts.find(prod => prod.id === prodId))
+    const editedProduct = useSelector(state =>
+        state.products.userProducts.find(prod => prod.id === prodId))
 
     //jeśli produkt istnieje to wstaw w inputa treść do edycji jeśli nie ma produktu to input jest pusty
     const [title, setTitle] = useState(editedProduct ? editedProduct.title : '')
     const [image, setImage] = useState(editedProduct ? editedProduct.imageUrl : '')
     const [price, setPrice] = useState(editedProduct ? editedProduct.price.toString() : '')
     const [description, setDescription] = useState(editedProduct ? editedProduct.description : '')
+
+    const dispatch = useDispatch()
+    //usecallback zapewni, że ta funkcja nie jest odtwarzana za każdym razem, gdy komponent jest renderowany
+    const handleSubmit = useCallback(() => {
+        if (editedProduct) {
+            //dodaję + przed price bo price jest typu number a do inputa wprowadzam stringa
+            dispatch(productActions.updateProduct(prodId, title, image, +price, description))
+        }
+        else {
+            dispatch(productActions.createProduct(title, image, +price, description))
+        }
+        //musze zdefiniować zależności, żeby funkcja wywoływała się gdy ulegnie zmianie cokolwiek, co jest
+        //edytowalne, czyli wszystko. gdyby to była pusta tablica nic by się nie tworzyło, bo usecallback nie
+        //zwracałby uwagi na żadne wprowadzone przez usera dane przy tworzeniu i edycji produktu
+    }, [dispatch, prodId, title, image, price, description])
+
+  //tworzę parametr, który odbiorę poza komponentem, w navigationOptions żeby zapisać edytowane zmiany w produkcie
+    useEffect(() => {
+        props.navigation.setParams({submit: handleSubmit})
+    }, [handleSubmit])
 
     return (
         <ScrollView>
@@ -61,16 +83,15 @@ const styles = StyleSheet.create({
 })
 
 EditProductScreen.navigationOptions = navData => {
+    const submit = navData.navigation.getParam('submit')
     return {
         headerTitle: navData.navigation.getParam('productId') ? 'Edycja produktu' : 'Dodaj produkt',
         headerRight: () =>
             <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
                 <Item
-                    title='Menu'
+                    title='Save'
                     iconName={Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'}
-                    onPress={() => {
-                        navData.navigation.navigate('EditProducts')
-                    }}
+                    onPress={submit}
                 />
             </HeaderButtons>,
     }
