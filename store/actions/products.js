5078aafa4394174d69cf5dbfbd572c03038f49bd
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-03-04 11:56:57 +0100

    Sending productData to the server after create

--- store/actions/products.js
+++ store/actions/products.js
@@ -7,18 +7,47 @@ export const deleteProduct = productId => {
 }
 
 export const createProduct = (title, imageUrl, price, description) => {
-    return {
-        type: CREATE_PRODUCT,
-        productData: {
-            title,
-            imageUrl,
-            price,
-            description
-        }
+    return async dispatch => {
+        //redux thunk pozwala na zwrócenie funkcji przez kreator akcji i podstawienie jej w miejsce pierwszego
+        //argumentu funkcji dispatch w komponencie który tę akcje dispatchuje
+        //mogę teraz zamieścic w tym miejscu dowolny kod asynchroniczny np żadanie HTTP np zapytanie do API
+        //wysyłam zapytanie do firebase i dodaję samodzielnie endpoint products a firebase sam utworzy folder w bazie
+        //w przypadku firebase trzeba jednak dodać na końcu .json
+        //fetch domyślnie wysyła zapytanie metodą get, ale do zapisania danych potrzeba metody post i dlatego wymagany
+        //jest drugi argument metody fetch
+        //używam nowoczesnej skłądni async await która zastępuje wykorzystanie .then(response => ...). Oznacza to, że
+        //funkcja zwracana przez createProduct jest asynchroniczna i program ma zaczekać na odpowiedź, która przychodzi
+        // z serwera po wysłaniu zapytania fetch i zapisać ją w stałej response.
+        //Trzeba też pamiętać, że funkcja async await zwraca Promise więc cała funkcja createProduct też zwraca promise
+        const response = await fetch('https://native-shop-app-d7b20-default-rtdb.firebaseio.com/products.json', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            //obiektem wysyłanym do bazy będzie obiekt json, ametoda stringify przekształca tablicę, lub obiekt JS
+            //właśnie w obiekt JSON. Produkt jest obiektem JS więc potrzeba przekształcić go w jsona
+            body: JSON.stringify({title, imageUrl, price, description})
+        })
+
+        //zapisuję sobie dane zwrócone przez firebase bo wysłaniu zapytania. Jest top też zadanie asynchroniczne więc
+        //dodaje await
+        const resData = await response.json()
+
+        //diapatch poniżej wykona się dopiero gdy wszystkie akcje oznaczone await sie wykonają.
+        dispatch({
+            type: CREATE_PRODUCT,
+            productData: {
+                id: resData.name,
+                title,
+                imageUrl,
+                price,
+                description
+            }
+        })
     }
 }
 
-export const updateProduct = (id,title, imageUrl, price, description) => {
+export const updateProduct = (id, title, imageUrl, price, description) => {
     return {
         type: UPDATE_PRODUCT,
         prodId: id,
