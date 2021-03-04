8de0269ebc130887fc6da908b105b50d7fa88621
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-03-04 18:13:50 +0100

    Catching errors and added loaders

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,20 +1,37 @@
-import React, {useEffect} from 'react'
-import {FlatList, Platform} from 'react-native'
+import React, {useState, useEffect, useCallback} from 'react'
+import {View, Text, ActivityIndicator, FlatList, Platform, StyleSheet, Button} from 'react-native'
 import {useDispatch} from "react-redux";
 import {HeaderButtons, Item} from 'react-navigation-header-buttons'
 import {useSelector} from "react-redux";
 import ProductItem from "../../components/ProductItem";
 import CustomHeaderButton from "../../components/UI/HeaderButton";
 import * as productActions from '../../store/actions/products'
+import Colors from '../../constants/Colors'
 
 const ProductsOverviewScreen = props => {
 
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState()
     const availableProducts = useSelector(state => state.products.availableProducts)
     const dispatch = useDispatch()
 
+    //useEffect nie może wzrócic promisa dlatego funkcja jaką przyjmuje useeffect nie może być async, ale można
+    //to obejść tak jak poniżej, wykonując asynchroniczną funkcję w środku
+    const loadProducts = useCallback(async () => {
+        setError(null)
+        setIsLoading(true)
+        try {
+            await dispatch(productActions.fetchProducts())
+        } catch (err) {
+            setError(err.message)
+        }
+
+        setIsLoading(false)
+    }, [dispatch, setIsLoading, setError])
+
     useEffect(() => {
-        dispatch(productActions.fetchProducts())
-    }, [dispatch])
+        loadProducts()
+    }, [dispatch, loadProducts])
 
     const renderProductItem = itemData => {
         return (
@@ -32,6 +49,31 @@ const ProductsOverviewScreen = props => {
                     })
                 }}
             />
+        )
+    }
+
+    if (error) {
+    return (
+        <View style={styles.centered}>
+            <Text>Coś poszło nie tak!</Text>
+            <Button title='Spróbuj ponownie' onPress={loadProducts} color={Colors.headerColor}/>
+        </View>
+    )
+    }
+
+    if (isLoading) {
+        return (
+            <View style={styles.centered}>
+                <ActivityIndicator size='large' color={Colors.headerColor}/>
+            </View>
+        )
+    }
+
+    if (!isLoading && availableProducts.length === 0) {
+        return (
+            <View style={styles.centered}>
+                <Text>Nie znaleziono żadnych produktów.</Text>
+            </View>
         )
     }
 
@@ -70,5 +112,13 @@ ProductsOverviewScreen.navigationOptions = navData => {
             </HeaderButtons>
     }
 }
+
+const styles = StyleSheet.create({
+    centered: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})
 
 export default ProductsOverviewScreen
