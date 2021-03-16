091496d355124542ec33a03a2cc4b821ddd69325
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2021-03-16 09:51:40 +0100

    Added common authenticate action

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,5 +1,12 @@
+import {AsyncStorage} from 'react-native'
+
 export const SIGNUP = 'SIGNUP'
 export const LOGIN = 'LOGIN'
+export  const AUTHENTICATE = 'AUTHENTICATE';
+
+export const authenticate = (token, userId) => {
+    return { type: AUTHENTICATE, token: token, userId: userId}
+}
 
 export const signup = (email, password) => {
     return async dispatch => {
@@ -30,7 +37,9 @@ export const signup = (email, password) => {
         const resData = await response.json()
 
         console.log(resData)
-        dispatch({type: SIGNUP, token: resData.idToken, userId: resData.localId})
+        dispatch(authenticate(resData.idToken, resData.localId))
+        const expirationDate = new Date(new Date().getTime() + parseInt(resData.expiresIn) * 1000)
+        saveDataToStorage(resData.idToken, resData.localId, expirationDate)
     }
 }
 
@@ -67,6 +76,17 @@ export const login = (email, password) => {
         const resData = await response.json()
         console.log(resData.idToken)
         console.log(resData.localId)
-        dispatch({type: LOGIN, token: resData.idToken, userId: resData.localId})
+        dispatch(authenticate(resData.idToken, resData.localId))
+        //zawojam date w date żeby przekształcić liczbę milisekund z powrotem w datę
+        const expirationDate = new Date(new Date().getTime() + parseInt(resData.expiresIn) * 1000)
+        saveDataToStorage(resData.idToken, resData.localId, expirationDate)
     }
+}
+
+const saveDataToStorage = (token, userId, expirationDate) => {
+    AsyncStorage.setItem('userData', JSON.stringify({
+        token: token,
+        userId: userId,
+        expDate: expirationDate.toISOString()
+    }))
 }
